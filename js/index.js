f833f579428e900e6c4c1e2f4be31a032b9a84dd
Mehdi Hammouda <hmd.mehdii@gmail.com>
2018-06-19 00:10:52 +0200

    js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
   setTimeout(function() {
     document.getElementById('test').className = 'slideDown';
-  }, 300);
+  }, 30);
 }, false);
